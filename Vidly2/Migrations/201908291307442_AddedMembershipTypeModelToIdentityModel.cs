b35caa1d0a450895bee24c7489fa57c1181b96d7
Simon Shinkins <s.shinkins@yahoo.co.uk>
2019-08-29 14:11:01 +0100

    Add project files.

---
+++ Vidly2/Migrations/201908291307442_AddedMembershipTypeModelToIdentityModel.cs
@@ -0,0 +1,29 @@
+namespace Vidly2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedMembershipTypeModelToIdentityModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.MembershipTypes",
+                c => new
+                    {
+                        Id = c.Byte(nullable: false),
+                        Name = c.String(),
+                        SignUpFee = c.Short(nullable: false),
+                        DurationInMonths = c.Byte(nullable: false),
+                        DiscountRate = c.Byte(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.MembershipTypes");
+        }
+    }
+}
---
+++ Vidly2/Migrations/201908291309494_AddedCustomerModelToIdentityModel.cs
@@ -0,0 +1,33 @@
+namespace Vidly2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedCustomerModelToIdentityModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 255),
+                        DoB = c.DateTime(),
+                        IsSubscribedToNewsletter = c.Boolean(nullable: false),
+                        MembershipTypeId = c.Byte(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.MembershipTypes", t => t.MembershipTypeId, cascadeDelete: true)
+                .Index(t => t.MembershipTypeId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Customers", "MembershipTypeId", "dbo.MembershipTypes");
+            DropIndex("dbo.Customers", new[] { "MembershipTypeId" });
+            DropTable("dbo.Customers");
+        }
+    }
+}
